b6c7c1b585b483cc652fa71418efb05b8ae0241c
thiiz <afffvitu@gmail.com>
2022-08-24 20:42:56 -0300

    AnimatePresence removed from home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,165 +4,162 @@ import Filters from '../components/home/filters/Filters'
 import style from '../styles/Products.module.css'
 import Image from 'next/image'
 import Product from '../public/img/products/product2.jpg'
-import AnimatePresence from 'framer-motion'
 
 
 export default function Home() {
   return (
-    <AnimatePresence>
-      <main className="page">
-        <Banner />
-        <Infos />
-        <div className={style.content} id='produtos'>
-          <Filters />
-          <section className={style.container}>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
-            <section className={style.productContainer}>
-              <div className={style.imageContainer}>
-                <Image className={style.product} src={Product} alt='Produto' height='450' />
-              </div>
-              <a href='#'>Roupa de Crochê azul</a>
-              <p>R$ 99.90</p>
-              <button className={style.buy} type='button'>comprar</button>
-            </section>
+    <main className="page">
+      <Banner />
+      <Infos />
+      <div className={style.content} id='produtos'>
+        <Filters />
+        <section className={style.container}>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
           </section>
-        </div>
-      </main>
-    </AnimatePresence>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+          <section className={style.productContainer}>
+            <div className={style.imageContainer}>
+              <Image className={style.product} src={Product} alt='Produto' height='450' />
+            </div>
+            <a href='#'>Roupa de Crochê azul</a>
+            <p>R$ 99.90</p>
+            <button className={style.buy} type='button'>comprar</button>
+          </section>
+        </section>
+      </div>
+    </main>
   )
 }
