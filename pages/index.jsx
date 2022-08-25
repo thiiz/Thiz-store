fea1f969b89cd6aa3cab6a6a7a939bd7c1c28d08
thiiz <afffvitu@gmail.com>
2022-08-25 03:11:20 -0300

    useEffect removed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,9 @@
 import Banner from "../components/home/banner/Banner";
 import Infos from '../components/home/infos/Infos'
 import Products from '../components/home/products/Products'
-import { usePresence } from "framer-motion"
-import { useEffect } from "react";
 
 
 export default function Home() {
-  const [isPresent, safeToRemove] = usePresence()
-  useEffect(() => {
-    !isPresent && setTimeout(safeToRemove, 1000)
-  }, [isPresent])
   return (
     <main className="page">
       <Banner />
