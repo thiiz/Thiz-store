07f5654f89b86ed757fcb40fd8cce58b44956203
thiiz <afffvitu@gmail.com>
2022-08-25 06:13:27 -0300

    api routes integrada com os produtos

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,26 +34,8 @@ export default function Home({ data }) {
   )
 }
 export async function getStaticProps() {
-  const data = [
-    { id: 1, title: 'Primeira roupa de crochê', price: '99,90', image: '/img/products/product2.jpg' },
-    { id: 2, title: 'Segunda roupa de crochê', price: '89,90', image: '/img/products/product2.jpg' },
-    { id: 4, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 5, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 6, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 7, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 8, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 9, title: 'Terceira roupa de crochê', price: '69,90', image: '/img/products/product2.jpg' },
-    { id: 10, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 11, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 12, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 13, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 14, title: 'Terceira roupa de crochê', price: '69,90', image: '/img/products/product2.jpg' },
-    { id: 15, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 16, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 17, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 18, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 19, title: 'Terceira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-  ];
+  const res = await fetch('http://localhost:3000/api/product-list');
+  const data = await res.json();
   return {
     props: {
       data: data,
