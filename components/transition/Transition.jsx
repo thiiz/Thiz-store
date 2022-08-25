e21ea7e7c6e7e0586e9b2e11ee25bb9bf2583597
thiiz <afffvitu@gmail.com>
2022-08-25 02:39:33 -0300

    page transition changed

--- components/transition/Transition.jsx
+++ components/transition/Transition.jsx
@@ -4,62 +4,47 @@ import { useRouter } from 'next/router'
 const Transition = ({ children }) => {
   const { asPath } = useRouter();
   const variants = {
-    scaleDown: {
-      scale: 0.8,
-      y: 100,
-      transition: {
-        duration: 0.4
-      }
-    },
-    out: {
-      x: "-100%",
-      transition: {
-        duration: 0.4,
-        delay: 0.5
-      }
-    },
-    in: {
-      scale: 0.8,
-      y: 100,
-      x: "100%",
-      transition: {
-        duration: 0.4
-      }
-    },
-    center: {
-      x: 0,
-      scale: 0.8,
-      transition: {
-        duration: 0.4
-      }
-    },
-    scaleUp: {
-      scale: 1,
+    inactive: {
+      opacity: 1,
       y: 0,
       transition: {
-        duration: 0.4,
-        delay: 0.5
+        duration: 0.6,
+        ease: 'easeInOut'
+      },
+    },
+    out: {
+      opacity: 0,
+      y: -100,
+      transition: {
+        duration: 0.6,
+        ease: 'easeInOut'
+      },
+    },
+    in: {
+      y: 100,
+      opacity: 0,
+      transition: {
+        duration: 0.6,
+        ease: 'easeInOut'
       }
     },
   };
   return (
-    <div className="background">
-      <AnimatePresence
-        initial={false}
-        exitBeforeEnter
-      >
-        <motion.div
-          key={asPath}
-          variants={variants}
-          initial="in"
-          animate={["center", "scaleUp"]}
-          exit={["scaleDown", "out"]}
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
+    <div className='background'>
+      <div className="effect-2">
+        <AnimatePresence initial={false} exitBeforeEnter>
+          <motion.div
+            key={asPath}
+            variants={variants}
+            initial="in"
+            animate="inactive"
+            exit="out"
+          >
+            {children}
+          </motion.div>
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
-
 export default Transition;
