9d84ef691b0dedc6c8c533d6b6bf6e630f05ccac
thiiz <afffvitu@gmail.com>
2022-08-26 07:24:05 -0300

    dato cms added

--- next.config.js
+++ next.config.js
@@ -8,6 +8,6 @@ module.exports = nextConfig
 
 module.exports = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['www.datocms-assets.com'],
   },
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import Banner from "../components/home/banner/Banner";
 import Infos from '../components/home/infos/Infos'
 import style from '../styles/Products.module.css'
 import Filters from '../components/home/filters/Filters'
+import { getAllProducts } from '../lib/dato-cms';
 
-function Home({ data }) {
+function Home({ products }) {
+  console.log(products)
   return (
     <main className="page">
       <Banner />
@@ -12,15 +14,15 @@ function Home({ data }) {
       <div className={`${style.content}`} id='produtos'>
         <Filters />
         <section className={style.container}>
-          {data.map((item) => {
+          {products.map((product) => {
             return (
-              <section key={item.id} className={style.productContainer}>
+              <section key={product.id} className={style.productContainer}>
                 <div>
                   <div className={style.imageContainer}>
-                    <Image className={style.product} src={item.image} alt='Produto' width='650' height='500px' />
+                    <Image className={style.product} src={product.image.url} alt='Produto' width='650px' height='500px' />
                   </div>
-                  <a href='#'>{item.title}</a>
-                  <p>R$ {item.price}</p>
+                  <a href='#'>{product.title}</a>
+                  <p>R$ {product.price}</p>
                   <button className={style.buy} type='button'>comprar</button>
                 </div>
               </section>
@@ -32,14 +34,12 @@ function Home({ data }) {
   )
 }
 export async function getStaticProps() {
-  const productsApi = process.env.PRODUCTS_API
-  const res = await fetch(productsApi)
-  const data = await res.json();
+  const products = await getAllProducts()
   return {
     props: {
-      data: data,
+      products,
     },
-    revalidate: 604800,
+    revalidate: 25,
   }
 }
 export default Home;
