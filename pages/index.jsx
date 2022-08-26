02b4c8395a66afd9214a205f96b014ba36c967d6
thiiz <afffvitu@gmail.com>
2022-08-26 03:51:30 -0300

    products data update

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,13 +34,13 @@ function Home({ data }) {
 export async function getStaticProps() {
   const data = [
     { id: 1, title: 'Primeira roupa de crochê', price: '99,90', image: '/img/products/product2.jpg' },
-    { id: 2, title: 'Segunda roupa de crochê', price: '89,90', image: '/img/products/product2.jpg' },
+    { id: 2, title: 'Segunda roupa de crochê', price: '89,90', image: 'https://res.cloudinary.com/dsdjwmqkk/image/upload/v1661471647/cld-sample-5.jpg' },
     { id: 4, title: 'Quarta roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 5, title: 'Quinta roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
+    { id: 5, title: 'Quinta roupa de crochê', price: '79,90', image: 'https://res.cloudinary.com/dsdjwmqkk/image/upload/v1661471647/cld-sample-5.jpg' },
     { id: 6, title: 'Sexta roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 7, title: 'Sétima roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
+    { id: 7, title: 'Sétima roupa de crochê', price: '79,90', image: 'https://res.cloudinary.com/dsdjwmqkk/image/upload/v1661471647/cld-sample-5.jpg' },
     { id: 8, title: 'Oitava roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
-    { id: 9, title: 'Nona roupa de crochê', price: '69,90', image: '/img/products/product2.jpg' },
+    { id: 9, title: 'Nona roupa de crochê', price: '69,90', image: 'https://res.cloudinary.com/dsdjwmqkk/image/upload/v1661471647/cld-sample-5.jpg' },
     { id: 10, title: 'Décima roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
     { id: 11, title: 'Décima primeira roupa de crochê', price: '79,90', image: '/img/products/product2.jpg' },
     { id: 12, title: 'Décima segunda de roupa crochê', price: '79,90', image: '/img/products/product2.jpg' },
