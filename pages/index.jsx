72fc79771303127b8d61ab1c4889834f1005fa97
thiiz <afffvitu@gmail.com>
2022-08-26 08:56:23 -0300

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,24 @@
+import { getAllProducts } from '../lib/dato-cms';
+import { useRouter } from 'next/router'
 import Image from 'next/image'
 import Banner from "../components/home/banner/Banner";
 import Infos from '../components/home/infos/Infos'
 import style from '../styles/Products.module.css'
 import Filters from '../components/home/filters/Filters'
-import { getAllProducts } from '../lib/dato-cms';
 
 function Home({ products }) {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(`/product/${products.slug}`)
+  }
   return (
     <main className="page">
       <Banner />
       <Infos />
       <div className={`${style.content}`} id='produtos'>
         <Filters />
-        <section className={style.container}>
+        <section className={style.container} onClick={handleClick}>
           {products.map((product) => {
             return (
               <section key={product.id} className={style.productContainer}>
@@ -22,6 +28,7 @@ function Home({ products }) {
                   </div>
                   <a href='#'>{product.title}</a>
                   <p>R$ {product.price}</p>
+                  <p>R$ {product.slug}</p>
                   <button className={style.buy} type='button'>comprar</button>
                 </div>
               </section>
@@ -38,7 +45,7 @@ export async function getStaticProps() {
     props: {
       products,
     },
-    revalidate: 60,
+    revalidate: 120,
   }
 }
 export default Home;
