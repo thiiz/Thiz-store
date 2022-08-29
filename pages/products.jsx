84129ba1bbe0e7a0a4363c320e9d6c0318bb4b66
thiiz <afffvitu@gmail.com>
2022-08-29 03:53:17 -0300

    teste api

--- pages/products.jsx
+++ pages/products.jsx
@@ -46,7 +46,8 @@ export function Products({ products }) {
 }
 
 export async function getStaticProps() {
-	const data = await fetch('https://mae-terra.vercel.app/api/productsApi')
+	const product = await fetch('https://mae-terra.vercel.app/api/productsApi')
+	const data = await product.json()
 	return {
 		props: {
 			products: data.map((data) => ({
