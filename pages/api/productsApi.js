7c31492a5c62546127330521aee7ec34c0ec4292
thiiz <afffvitu@gmail.com>
2022-08-29 03:42:39 -0300

    update

--- pages/api/productsApi.js
+++ pages/api/productsApi.js
@@ -2,7 +2,6 @@ import { getAllProducts } from '../../lib/dato-cms'
 
 
 export default async function handler(req, res) {
-	console.log('chamou api')
 	const data = await getAllProducts()
 	if (req.method === 'GET') {
 		res.status(200).json(data)
