0ec5aea3bb08a43515c2e3d93a0e5ac69de84ff2
thiiz <afffvitu@gmail.com>
2022-08-31 09:02:58 -0300

    search products functional added

--- components/products/productView.jsx
+++ components/products/productView.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import ProductsItems from './productItems';
 import style from '../../styles/Products.module.css'
 
 
 export default function ProductView({ products }) {
-	const product = products.map(product => product)
-	const [stock, setStock] = useState(product)
 	return (
 
 		<div className={`${style.content}`} id='produtos'>
