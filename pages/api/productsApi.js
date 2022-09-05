16a25b7c33a0368d060b193c30dc8256d14800e7
thiiz <afffvitu@gmail.com>
2022-09-05 06:50:45 -0300

    added secret key for api routes products

--- pages/api/productsApi.js
+++ pages/api/productsApi.js
@@ -1,9 +1,9 @@
 import { getData } from '../../lib/queries'
 
 export default async function handler(req, res) {
-	// Check for secret to confirm this is a valid request
-	if (req.method === 'GET') {
+	console.log('req query', req.query.key)
+	if (req.query.key === process.env.SECRET_KEY) {
 		const data = await getData()
 		return res.status(200).json(data.data.allProducts)
-	  } 
+	}
 }
