e2e76d538785bd50ac9c66dd498ae2640cbd9bc1
thiiz <afffvitu@gmail.com>
2022-09-05 07:36:18 -0300

    teste

--- pages/api/productsApi.js
+++ pages/api/productsApi.js
@@ -1,7 +1,7 @@
 import { getData } from '../../lib/queries'
 
 export default async function handler(req, res) {
-	if (req.query.key === process.env.SECRET_KEY) {
+	if (req.query.secret_key === process.env.SECRET_KEY) {
 		const data = await getData()
 		return res.status(200).json(data.data.allProducts)
 	} else {
