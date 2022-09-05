75dacdb606cd406e887d295149eec444691f6f22
thiiz <afffvitu@gmail.com>
2022-09-05 07:45:37 -0300

    update secret api

--- pages/api/productsApi.js
+++ pages/api/productsApi.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
 		const data = await getData()
 		return res.status(200).json(data.data.allProducts)
 	} else {
-		const data = await getData()
-		return res.status(201).json(data.data.allProducts)
+		return res.status(403).json('ERROR: USER NOT ALLOWED')
 	}
 }
