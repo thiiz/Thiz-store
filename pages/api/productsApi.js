94200448271290d919b13c985d884d66daf20a1a
thiiz <afffvitu@gmail.com>
2022-09-05 07:21:06 -0300

    env secret key removed for tests

--- pages/api/productsApi.js
+++ pages/api/productsApi.js
@@ -1,9 +1,10 @@
 import { getData } from '../../lib/queries'
 
 export default async function handler(req, res) {
-	console.log('req query', req.query.key)
 	if (req.query.key === process.env.SECRET_KEY) {
 		const data = await getData()
 		return res.status(200).json(data.data.allProducts)
+	} else {
+		return res.status(401).json('ERROR: USER NOT ALLOWED')
 	}
 }
