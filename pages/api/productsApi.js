63c005590aa3232217bc4043219a9af32b169ae1
thiiz <afffvitu@gmail.com>
2022-09-05 07:28:19 -0300

    teste

--- pages/api/productsApi.js
+++ pages/api/productsApi.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
 		const data = await getData()
 		return res.status(200).json(data.data.allProducts)
 	} else {
-		return res.status(401).json('ERROR: USER NOT ALLOWED')
+		const data = await getData()
+		return res.status(201).json(data.data.allProducts)
 	}
 }
