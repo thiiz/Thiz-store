000bb77a634132cac33ab63c4b408eee35f69562
thiiz <afffvitu@gmail.com>
2022-09-06 16:15:44 -0300

    fixing...

--- components/load-screen/loadScreen.jsx
+++ components/load-screen/loadScreen.jsx
@@ -1,6 +1,6 @@
 import style from './loadscreen.module.css'
 
-export default function loadScreen() {
+export default function LoadScreen() {
 	return (
 		<div className={style.loading}>
 			<div className={style.spinner}></div>
