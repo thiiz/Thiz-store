c903f6c12f8e95970f01e338a9f14429e155f516
thiiz <afffvitu@gmail.com>
2022-09-14 16:05:13 -0300

    update

--- components/loading-screen/loadingScreen.jsx
+++
@@ -1,9 +0,0 @@
-import style from './LoadingScreen.module.css'
-
-export default function LoadingScreen() {
-	return (
-		<div className={style.loading}>
-			<div className={style.spinner}></div>
-		</div>
-	)
-}
--- layout/header/Header.jsx
+++ layout/header/Header.jsx
@@ -8,28 +8,25 @@ import { MdHeadsetMic } from 'react-icons/md'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
 import { motion } from "framer-motion"
 import { useMenuCart } from '../../contexts/OpenCartMenuContext'
-
+import { useScrollDirection } from '../../lib/useScrollDirection'
+import { useIsSmall, useIsMedium } from '../../lib/useMediaQuery'
 
 function Header() {
+
 	const { isOpen, setIsOpen } = useMenuCart()
-	const [small, setSmall] = useState(false);
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			window.addEventListener("scroll", () =>
-				setSmall(window.pageYOffset > 200)
-			);
-		}
-		return () => {
-			window.removeEventListener('scroll');
-		};
-	}, [])
+	const [isMobile, setIsMobile] = useState(false);
+	const scrollDirection = useScrollDirection()
+
+	const isMedium = useIsMedium()
+	const isSmall = useIsSmall()
+
 	const cartVariant = {
 		open: { opacity: 1, x: 0 },
 		closed: { opacity: 0, x: "100%" },
 	}
-	const headerVariant = {
-		small: { height: '55px', padding: '0 295px 0 295px'},
-		normal: { height: "80px"},
+	const headerVariant = isSmall ? {
+		small: { height: '55px', padding: '0 15px' },
+		normal: { height: "85px", padding: '0 15px' },
 
 		small_Logo: { width: '50px' },
 		normal_Logo: { width: '72px' },
@@ -42,34 +39,63 @@ function Header() {
 
 		small_Button: { fontSize: '20px' },
 		normal_Button: { fontSize: '24px' },
-		
+	} : isMedium ? {
+		small: { height: '55px', padding: '0 25px' },
+		normal: { height: "85px", padding: '0 15px' },
+
+		small_Logo: { width: '49px' },
+		normal_Logo: { width: '62px' },
+
+		small_Font: { fontSize: '16px' },
+		normal_Font: { fontSize: '20px' },
+
+		small_Menu: { fontSize: '20px' },
+		normal_Menu: { fontSize: '24px' },
+
+		small_Button: { fontSize: '18px' },
+		normal_Button: { fontSize: '22px' },
+	} : {
+		small: { height: '55px', padding: '0 80px' },
+		normal: { height: "85px", padding: '0 80px' },
+
+		small_Logo: { width: '50px' },
+		normal_Logo: { width: '72px' },
+
+		small_Font: { fontSize: '20px' },
+		normal_Font: { fontSize: '24px' },
+
+		small_Menu: { fontSize: '22px' },
+		normal_Menu: { fontSize: '26px' },
+
+		small_Button: { fontSize: '20px' },
+		normal_Button: { fontSize: '24px' },
 	}
 	return (
 		<>
-			<motion.header drag={true} className={style.header} animate={small ? "small" : "normal"} variants={headerVariant} transition={{ ease: "easeOut", duration: 0.3 }}>
-				<motion.div animate={small ? "small_Logo" : "normal_Logo"} variants={headerVariant}>
+			<motion.header drag={true} className={style.header} animate={scrollDirection === "down" ? "small" : "normal"} variants={headerVariant} transition={{ ease: "easeOut", duration: 0.3 }}>
+				<motion.div animate={scrollDirection === "down" ? "small_Logo" : "normal_Logo"} variants={headerVariant}>
 					<Link href="/"><a><Image className={style.logo} src='/logo-maeTerra2.png' alt='logo-natureza' height='77px' width='77px'></Image></a></Link>
 				</motion.div>
 				<div className={style.menuBtn}>
 					<ul>
-						<li><Link href="/"><motion.a animate={small ? "small_Font" : "normal_Font"} variants={headerVariant}>início</motion.a></Link></li>
-						<li><Link href='/man'><motion.a animate={small ? "small_Font" : "normal_Font"} variants={headerVariant}>masculino</motion.a></Link></li>
-						<li><Link href="/woman"><motion.a animate={small ? "small_Font" : "normal_Font"} variants={headerVariant}>feminino</motion.a></Link></li>
-						<li><Link href='/about'><motion.a animate={small ? "small_Font" : "normal_Font"} variants={headerVariant}>sobre</motion.a></Link></li>
-						<li><Link href="/contact"><motion.a animate={small ? "small_Font" : "normal_Font"} variants={headerVariant}>contato</motion.a></Link></li>
+						<li><Link href="/"><motion.a animate={scrollDirection === "down" ? "small_Font" : "normal_Font"} variants={headerVariant}>início</motion.a></Link></li>
+						<li><Link href='/man'><motion.a animate={scrollDirection === "down" ? "small_Font" : "normal_Font"} variants={headerVariant}>masculino</motion.a></Link></li>
+						<li><Link href="/woman"><motion.a animate={scrollDirection === "down" ? "small_Font" : "normal_Font"} variants={headerVariant}>feminino</motion.a></Link></li>
+						<li><Link href='/about'><motion.a animate={scrollDirection === "down" ? "small_Font" : "normal_Font"} variants={headerVariant}>sobre</motion.a></Link></li>
+						<li><Link href="/contact"><motion.a animate={scrollDirection === "down" ? "small_Font" : "normal_Font"} variants={headerVariant}>contato</motion.a></Link></li>
 					</ul>
 				</div>
 				<div className={style.containerBtn}>
 					<section className={style.btnInfoContainer}>
-						<motion.button animate={small ? "small_Menu" : "normal_Menu"} variants={headerVariant} className={`${style.btn} ${style.btnInfo}`} type='button'><MdHeadsetMic /></motion.button>
+						<motion.button animate={scrollDirection === "down" ? "small_Menu" : "normal_Menu"} variants={headerVariant} className={`${style.btn} ${style.btnInfo}`} type='button'><MdHeadsetMic /></motion.button>
 						<div>
-							<motion.button animate={small ? "small_Menu" : "normal_Menu"} variants={headerVariant} onClick={() => setIsOpen(isOpen => !isOpen)} className={`${style.btn} ${style.btnInfo}`} type='button'>
+							<motion.button animate={scrollDirection === "down" ? "small_Menu" : "normal_Menu"} variants={headerVariant} onClick={() => setIsOpen(isOpen => !isOpen)} className={`${style.btn} ${style.btnInfo}`} type='button'>
 								<FaShoppingCart />
-								<div className={`${style.countCartItems} ${small ? style.countCartitemsSmall : style.countCartitemsNormal}`}>0</div>
+								<div className={`${style.countCartItems} ${scrollDirection === "down" ? style.countCartitemsSmall : style.countCartitemsNormal}`}>0</div>
 							</motion.button>
 						</div>
 						<section className={style.btnLoginSpace}>
-							<button className={`${style.btn} ${style.btnLogin} ${small ? style.btnLoginSmall : style.btnLoginNormal}`} type='button'>LOGIN</button>
+							<button className={`${style.btn} ${style.btnLogin} ${scrollDirection === "down" ? style.btnLoginSmall : style.btnLoginNormal}`} type='button'>LOGIN</button>
 						</section>
 					</section>
 				</div>
