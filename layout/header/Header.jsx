5f54becdeb2eee9473b334fc201023df3b4f4851
thiiz <afffvitu@gmail.com>
2022-09-17 19:02:36 -0300

    header fixed

--- layout/header/Header.jsx
+++ layout/header/Header.jsx
@@ -11,63 +11,58 @@ import { useMenuCart } from '../../contexts/OpenCartMenuContext'
 import { useScrollDirection } from '../../lib/useScrollDirection'
 import { useDesktopSize } from '../../lib/useAnimate'
 import { useMobileSize } from '../../lib/useAnimate'
-import { useIsMedium, useIsSmall } from '../../lib/MediaQuery'
+import { useIsSmall } from '../../lib/MediaQuery'
 
 
 function Header() {
-	const medium = useIsMedium()
 	const small = useIsSmall()
 	const { isOpen, setIsOpen } = useMenuCart()
 	const [isOpenMobile, setIsOpenMobile] = useState(false)
-	const [isMobile, setIsMobile] = useState(small);
 	const scrollDirection = useScrollDirection()
 	const desktopVariant = useDesktopSize()
-	const MobileVariant = useMobileSize()
+	const mobileVariant = useMobileSize()
+
+	console.log(mobileVariant)
 
 
-	const cartVariant = {
+	const closeVariant = {
 		open: { opacity: 1, x: 0 },
 		closed: { opacity: 0, x: "100%" },
 	}
 
-
-	useEffect(() => {
-		setIsMobile(medium)
-	}, [medium])
-
 	return (
 		<>
-			<motion.header className={style.header} animate={!small ? scrollDirection === "down" ? "small" : "normal" : isOpenMobile ? "normal" : "small"} variants={desktopVariant} transition={{ ease: "easeOut", duration: 0.3 }}>
-				{small ? <button onClick={() => setIsOpenMobile(isOpenMobile => !isOpenMobile)}>X</button> : ''}
-				<motion.div animate={!small ? scrollDirection === "down" ? "small_Logo" : "normal_Logo" : "normal_Logo"} variants={desktopVariant}>
-					<Link href="/"><a><Image className={style.logo} src='/logo-maeTerra2.png' alt='logo-natureza' height='77px' width='77px'></Image></a></Link>
-				</motion.div>
-				<div className={style.menuBtn}>
-					<ul >
-						<li><Link href="/"><motion.a animate={!small ? scrollDirection === "down" ? "small_Font" : "normal_Font" : ''} variants={!isMobile ? desktopVariant : ''}>início</motion.a></Link></li>
-						<li><Link href='/products'><motion.a animate={!small ? scrollDirection === "down" ? "small_Font" : "normal_Font" : ''} variants={!isMobile ? desktopVariant : ''}>produtos</motion.a></Link></li>
-						<li><Link href='/about'><motion.a animate={!small ? scrollDirection === "down" ? "small_Font" : "normal_Font" : ''} variants={!isMobile ? desktopVariant : ''}>sobre</motion.a></Link></li>
-						<li><Link href="/contact"><motion.a animate={!small ? scrollDirection === "down" ? "small_Font" : "normal_Font" : ''} variants={!isMobile ? desktopVariant : ''}>contato</motion.a></Link></li>
-					</ul>
+			<motion.header className={style.header} animate={!small ? scrollDirection === "down" ? "small" : "normal" : isOpenMobile ? "normal" : "small"} variants={!small ? desktopVariant : mobileVariant} transition={{ ease: "easeOut", duration: 0.3 }}>
+				<div className={style.taggleMenuMobile}>
+					<motion.div animate={!small ? scrollDirection === "down" ? "small_Logo" : "normal_Logo" : "normal_Logo"} variants={small ? mobileVariant : desktopVariant}>
+						<Link href="/"><a><Image className={style.logo} src='/logo-maeTerra2.png' alt='logo-natureza' height='77px' width='77px'></Image></a></Link>
+					</motion.div>
+					{small ? <button onClick={() => setIsOpenMobile(isOpenMobile => !isOpenMobile)}>X</button> : ''}
 				</div>
-				<div className={!isMobile ? style.containerBtn : style.containerBtnMobile}>
+				<motion.div className={style.menuBtn} animate={isOpenMobile ? "open_Menu" : "closed_Menu"} variants={mobileVariant}>
+						<ul className={style.NavMenuList}>
+							<li><Link href="/"><motion.a animate={!small ? scrollDirection === "down" || small ? "small_Font" : "normal_Font" : ''} variants={!small ? desktopVariant : mobileVariant}>início</motion.a></Link></li>
+							<li><Link href='/products'><motion.a animate={!small ? scrollDirection === "down" || small ? "small_Font" : "normal_Font" : ''} variants={!small ? desktopVariant : mobileVariant}>produtos</motion.a></Link></li>
+							<li><Link href='/about'><motion.a animate={!small ? scrollDirection === "down" || small ? "small_Font" : "normal_Font" : ''} variants={!small ? desktopVariant : mobileVariant}>sobre</motion.a></Link></li>
+							<li><Link href="/contact"><motion.a animate={!small ? scrollDirection === "down" || small ? "small_Font" : "normal_Font" : ''} variants={!small ? desktopVariant : mobileVariant}>contato</motion.a></Link></li>
+						</ul>
+				</motion.div>
+				<motion.div className={!small ? style.containerBtn : style.containerBtnMobile} animate={isOpenMobile ? "open_Menu" : "closed_Menu"} variants={mobileVariant} transition={{ ease: "easeOut", duration: 0.4 }}>
 					<section className={style.btnInfoContainer}>
 						<motion.button animate={!small ? scrollDirection === "down" ? "small_Menu" : "normal_Menu" : ''} variants={desktopVariant} className={`${style.btn} ${style.btnInfo}`} type='button'><MdHeadsetMic /></motion.button>
-						<div>
-							<motion.button animate={!small ? scrollDirection === "down" ? "small_Menu" : "normal_Menu" : ''} variants={desktopVariant} onClick={() => setIsOpen(isOpen => !isOpen)} className={`${style.btn} ${style.btnInfo}`} type='button'>
-								<FaShoppingCart />
-								<div className={`${style.countCartItems} ${!small ? scrollDirection === "down" ? style.countCartitemsSmall : style.countCartitemsNormal : style.countCartitemsNormal}`}>0</div>
-							</motion.button>
-						</div>
+						<motion.button animate={!small ? scrollDirection === "down" ? "small_Menu" : "normal_Menu" : ''} variants={desktopVariant} onClick={() => setIsOpen(isOpen => !isOpen)} className={`${style.btn} ${style.btnInfo}`} type='button'>
+							<FaShoppingCart />
+							<div className={`${style.countCartItems} ${!small ? scrollDirection === "down" ? style.countCartitemsSmall : style.countCartitemsNormal : style.countCartitemsNormal}`}>0</div>
+						</motion.button>
 						<div className={style.btnSpace}>
 							<button className={`${style.btn} ${style.btnLogin} ${!small ? scrollDirection === "down" ? style.btnLoginSmall : style.btnLoginNormal : ''}`} type='button'>LOGIN</button>
 						</div>
 					</section>
-				</div>
+				</motion.div>
 			</motion.header>
 			<motion.nav
 				animate={isOpen ? "open" : "closed"}
-				variants={cartVariant}
+				variants={closeVariant}
 				className={style.cart}
 			>
 				<button onClick={() => setIsOpen(isOpen => !isOpen)} className={style.closeBtn}><AiOutlineCloseCircle /><p className={style.closeText}>Fechar</p></button>
