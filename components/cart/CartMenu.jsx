6131edf6fa47b8f2aa55bfa479a79e8d8c5132be
thiiz <afffvitu@gmail.com>
2022-09-21 07:16:45 -0300

    fixing close button

--- components/cart/CartMenu.jsx
+++ components/cart/CartMenu.jsx
@@ -2,24 +2,26 @@ import style from './Cart.module.css'
 import CartItems from './CartItems'
 import { useCart } from '../../contexts/CartContext'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
-import {useMenuCart} from '../../contexts/OpenCartMenuContext'
+import { useMenuCart } from '../../contexts/OpenCartMenuContext'
 
 export function CartMenu() {
 	const { setIsOpen } = useMenuCart()
 	const { cart, totalPrice } = useCart()
 	return (
-		<div className={style.container}>
+		<>
 			<button onClick={() => setIsOpen(false)} className={style.closeBtn}><AiOutlineCloseCircle /><p className={style.closeText}>Fechar</p></button>
-			<div className={style.product}>
-				<p className={style.titleMyCart}>meu carrinho</p>
+			<div className={style.container}>
+				<div className={style.product}>
+					<p className={style.titleMyCart}>meu carrinho</p>
 
-				{cart?.map((item) => {
-					return (
-						<CartItems key={item.id} item={item} />
-					);
-				})}
-				<div>total: {totalPrice}</div>
-			</div>
-		</div >
+					{cart?.map((item) => {
+						return (
+							<CartItems key={item.id} item={item} />
+						);
+					})}
+					<div>total: {totalPrice}</div>
+				</div>
+			</div >
+		</>
 	)
 } 
