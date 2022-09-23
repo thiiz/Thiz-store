8b1a2edc89dc1ea299aff4c70fd2b9b24c6e84a0
thiiz <afffvitu@gmail.com>
2022-09-23 06:05:12 -0300

    products component reformated

--- components/products/productItems.jsx
+++ components/products/productItems.jsx
@@ -14,23 +14,24 @@ export default function ProductsItems({ stock }) {
 	const { add } = useCart()
 	const { notifyCart } = useNotify()
 	const price = `${stock?.price.toString().replace(".", ",")}0`;
-	const parcel = (Math.round(stock?.price / 6 * 100))/100.0;
+	const calc = (Math.round(stock?.price / 6 * 100)) / 100.0;
+	const parcel = calc.toString().replace(".", ",");
 	const title = () => {
-		if (stock?.title.length > 34) {
-		   return stock.title.substring(0, 35) + '...';
+		if (stock?.title.length > 36) {
+			return 
 		}
 		return stock.title;
-	 }
+	}
 	return (
 		<div className={style.productContainer}>
-				<div className={style.imageContainer} onClick={handleViewProduct}>
-					<Image className={style.productImg} data={stock?.image.responsiveImage} alt={`produto: ${stock?.image.alt}`}/>
-				</div>
-				<p onClick={handleViewProduct} className={style.title}>{title()}</p>
-				<p className={style.price}>R$ <strong>{price}</strong></p>
-				<p className={style.parcel}>OU 6X R$ {parcel}</p>
-				{stock?.instock !== 0 && <button onClick={() => { add(stock), notifyCart() }} className={style.buy} type='button'>comprar</button>}
-				{stock?.instock === 0 && <button className={style.unavailable} type='button' disabled>indisponível</button>}
+			<div onClick={handleViewProduct} className={style.imageContainer} >
+				<Image className={style.productImg} data={stock?.image.responsiveImage} alt={`produto: ${stock?.image.alt}`} />
+			</div>
+			<p onClick={handleViewProduct} className={style.title}>{stock.title}</p>
+			<p className={style.price}><strong>R$ {price}</strong></p>
+			<p className={style.parcel}>OU 6X <strong>R$ {parcel}</strong></p>
+			{stock?.instock !== 0 && <button onClick={() => { add(stock), notifyCart() }} className={style.buy} type='button'>comprar</button>}
+			{stock?.instock === 0 && <button className={style.unavailable} type='button' disabled>indisponível</button>}
 		</div>
 	)
 }
