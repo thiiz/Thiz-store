96f8b2dd59693661b99a386abe8f2ec4e7752c47
thiiz <afffvitu@gmail.com>
2022-10-04 18:53:04 -0300

    shop button modify

--- components/home/banner/Banner.jsx
+++ components/home/banner/Banner.jsx
@@ -7,9 +7,14 @@ export default function Banner() {
 
 	return (
 		<div className={`${style.bannerContainer}`}>
-				<h1 className={style.mainTitle}>MAIN TITLE <span>HERE</span></h1>
-				<h2 className={style.mainSubTitle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
-				<Link to="filterProducts" spy={true} smooth={true} offset={-70} duration={400} className={`${style.btn} ${style.btnShop}`} type='button'>SHOP</Link >
+			<h1 className={style.mainTitle}>MAIN TITLE <span>HERE</span></h1>
+			<h2 className={style.mainSubTitle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+			<Link to="filterProducts" spy={true} smooth={true} offset={-70} duration={400} className={`${style.btn} ${style.btnShop}`} type='button'>
+				<span className={style.topKey}></span>
+				<span className={style.btnText}>shop</span>
+				<span className={style.bottomKey1}></span>
+				<span className={style.bottomKey2}></span>
+			</Link >
 		</div>
 	)
 
