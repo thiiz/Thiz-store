ff4ff5cf78c29f7aa254b27a5d6f572941e0a103
thiiz <afffvitu@gmail.com>
2022-10-29 02:16:40 -0300

    _document test

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html>
+		<Html lang='pt-br'>
+			<script>0</script>
 			<Head>
 				<meta charSet="utf-8" />
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="description" content="Loja de Crochê - MãeTerra"></meta>
-				<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;600&family=Oswald:wght@200;400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,500&family=Varela+Round&display=swap" rel="stylesheet"/>			</Head>
+				<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;600&family=Oswald:wght@200;400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,500&family=Varela+Round&display=swap" rel="stylesheet" />			</Head>
 			<body>
 				<Main />
 				<NextScript />
