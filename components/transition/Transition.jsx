0afdb18dfe2392ebdf3d4eacb470acb5210eec56
thiiz <afffvitu@gmail.com>
2022-10-30 19:26:46 -0300

    transition t

--- components/transition/Transition.jsx
+++ components/transition/Transition.jsx
@@ -3,22 +3,19 @@ import { useRouter } from 'next/router'
 
 const Transition = ({ children }) => {
   const { asPath } = useRouter();
-  const variants = {
-    hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
-  }
   return (
-    <motion.main
-      variants={variants} // Pass the variant object into Framer Motion 
-      initial="hidden"
-      key={asPath} // Set the initial state to variants.hidden
-      animate="enter" // Animated state to variants.enter
-      exit="exit" // Exit state (used later) to variants.exit
-      transition={{ type: 'linear' }} // Set the transition to linear
-    >
-      {children}
-    </motion.main>
+    <AnimatePresence initial={false} mode='wait'>
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        key={asPath}
+        transition={{ duration: .5 }}
+        className="main"
+      >
+        {children}
+      </motion.main>
+    </AnimatePresence>
   );
 };
 export default Transition;
