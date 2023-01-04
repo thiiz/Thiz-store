ecafec73627b3dad28236e8f02e63988c3d595ad
thiiz <afffvitu@gmail.com>
2023-01-04 11:26:26 -0300

    html email verify recover code

--- utils/sendEmailCodeRecover.js
+++ utils/sendEmailCodeRecover.js
@@ -16,14 +16,257 @@ export default async function SendEmailCode(email, { code, name }) {
 		subject: "Seu código de verificação do ecommerce para redefinir a senha.", // Subject line
 		text: `${code}`, // plain text body
 		html: `
-		<body style="margin:0;padding:0;">
-			<p>
-				olá, <span style="color: #0352fc">${name},</span>
-			</p>
-			<span> Alguém está tentando alterar a senha no Ecommerce. </span><span>Insira o seguinte código para continuar: <strong>${code}</strong>
-			<p>
-				<span style="fontStyle: italic, fontWeight: bold">Observe que este código expirará em ${new Date(date.setHours(date.getHours() + 1))}</span>
-			</p>
-		</body > `,
+		<html>
+			<head>
+				<meta charset="utf-8">
+				<meta http-equiv="x-ua-compatible" content="ie=edge">
+				<title>Email Confirmation</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<style type="text/css">
+				/**
+				 * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
+				 */
+				@media screen {
+					@font-face {
+					font-family: 'Source Sans Pro';
+					font-style: normal;
+					font-weight: 400;
+					src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+					}
+
+					@font-face {
+					font-family: 'Source Sans Pro';
+					font-style: normal;
+					font-weight: 700;
+					src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+					}
+				}
+
+				/**
+				 * Avoid browser level font resizing.
+				 * 1. Windows Mobile
+				 * 2. iOS / OSX
+				 */
+				body,
+				table,
+				td,
+				a {
+					-ms-text-size-adjust: 100%; /* 1 */
+					-webkit-text-size-adjust: 100%; /* 2 */
+				}
+
+				/**
+				 * Remove extra space added to tables and cells in Outlook.
+				 */
+				table,
+				td {
+					mso-table-rspace: 0pt;
+					mso-table-lspace: 0pt;
+				}
+
+				/**
+				 * Better fluid images in Internet Explorer.
+				 */
+				img {
+					-ms-interpolation-mode: bicubic;
+				}
+
+				/**
+				 * Remove blue links for iOS devices.
+				 */
+				a[x-apple-data-detectors] {
+					font-family: inherit !important;
+					font-size: inherit !important;
+					font-weight: inherit !important;
+					line-height: inherit !important;
+					color: inherit !important;
+					text-decoration: none !important;
+				}
+
+				/**
+				 * Fix centering issues in Android 4.4.
+				 */
+				div[style*="margin: 16px 0;"] {
+					margin: 0 !important;
+				}
+
+				body {
+					width: 100% !important;
+					height: 100% !important;
+					padding: 0 !important;
+					margin: 0 !important;
+				}
+
+				/**
+				 * Collapse table borders to avoid space between cells.
+				 */
+				table {
+					border-collapse: collapse !important;
+				}
+
+				a {
+					color: #1a82e2;
+				}
+
+				img {
+					height: auto;
+					line-height: 100%;
+					text-decoration: none;
+					border: 0;
+					outline: none;
+				}
+				</style>
+			</head>
+			
+	<body style="background-color: #e9ecef;">
+
+	<!-- start preheader -->
+	<div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+		Alguém está tentando alterar a senha no Ecommerce
+	</div>
+	<!-- end preheader -->
+
+	<!-- start body -->
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+
+		<!-- start logo -->
+		<tr>
+		<td align="center" bgcolor="#e9ecef">
+			<!--[if (gte mso 9)|(IE)]>
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+			<tr>
+			<td align="center" valign="top" width="600">
+			<![endif]-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+			<tr>
+				<td align="center" valign="top" style="padding: 36px 24px;">
+				<a href="https://mae-terra.vercel.app" target="_blank" style="display: inline-block;">
+					<img src="https://mae-terra.vercel.app/_next/image?url=%2Flogo-maeTerra2.png&w=96&q=75" alt="Logo" border="0" width="86" style="display: block; width: 86px; max-width: 86px; min-width: 48px;">
+				</a>
+				</td>
+			</tr>
+			</table>
+			<!--[if (gte mso 9)|(IE)]>
+			</td>
+			</tr>
+			</table>
+			<![endif]-->
+		</td>
+		</tr>
+		<!-- end logo -->
+
+		<!-- start hero -->
+		<tr>
+		<td align="center" bgcolor="#e9ecef">
+			<!--[if (gte mso 9)|(IE)]>
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+			<tr>
+			<td align="center" valign="top" width="600">
+			<![endif]-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+			<tr>
+				<td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+				<h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Redefinir a senha</h1>
+				</td>
+			</tr>
+			</table>
+			<!--[if (gte mso 9)|(IE)]>
+			</td>
+			</tr>
+			</table>
+			<![endif]-->
+		</td>
+		</tr>
+		<!-- end hero -->
+
+		<!-- start copy block -->
+		<tr>
+		<td align="center" bgcolor="#e9ecef">
+			<!--[if (gte mso 9)|(IE)]>
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+			<tr>
+			<td align="center" valign="top" width="600">
+			<![endif]-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+
+			<!-- start copy -->
+			<tr>
+				<td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+				<p style="margin: 0;">Alguém está tentando alterar a senha no Ecommerce.</p>
+				<p style="margin: 0;">Digite o código a seguir para continuar:</p>
+				</td>
+			</tr>
+			<!-- end copy -->
+
+			<!-- start button -->
+			<tr>
+				<td align="left" bgcolor="#ffffff">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%">
+					<tr>
+					<td align="center" bgcolor="#ffffff" style="padding: 12px;">
+						<table border="0" cellpadding="0" cellspacing="0">
+						<tr>
+							<td align="center" bgcolor="#faf9fa" style="border-radius: 6px;">
+							<span style="letter-spacing: 10px; display: inline-block; padding: 14px 34px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 24px; color: #000; border: 1px solid #dad8de; border-radius: 6px;">${code}</span>
+							</td>
+						</tr>
+						</table>
+					</td>
+					</tr>
+				</table>
+				</td>
+			</tr>
+			<!-- end button -->
+
+			<!-- start copy -->
+			<tr>
+				<td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 24px;">
+				<p style="margin: 0;">Observe que este código vai expirar em ${new Date(date.setHours(date.getHours() + 1))}</p>
+				</td>
+			</tr>
+			<!-- end copy -->
+
+			</table>
+			<!--[if (gte mso 9)|(IE)]>
+			</td>
+			</tr>
+			</table>
+			<![endif]-->
+		</td>
+		</tr>
+		<!-- end copy block -->
+
+		<!-- start footer -->
+		<tr>
+		<td align="center" bgcolor="#e9ecef" style="padding: 24px;">
+			<!--[if (gte mso 9)|(IE)]>
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+			<tr>
+			<td align="center" valign="top" width="600">
+			<![endif]-->
+			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+
+			<!-- start permission -->
+			<tr>
+				<td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+					<p style="margin: 0;">Você recebeu este e-mail porque recebemos uma solicitação para redefinir a senha. Se você não solicitou, pode excluir este e-mail com segurança.</p>
+				</td>
+			</tr>
+			<!-- end permission -->
+
+			</table>
+			<!--[if (gte mso 9)|(IE)]>
+			</td>
+			</tr>
+			</table>
+			<![endif]-->
+		</td>
+		</tr>
+		<!-- end footer -->
+
+	</table>
+	<!-- end body -->
+	</body>
+</html>`,
 	})
 }	
