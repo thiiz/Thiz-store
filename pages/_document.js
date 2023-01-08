b80003c81d271b37ec0fe1176d6fec3becfcfd0a
thiiz <afffvitu@gmail.com>
2023-01-08 18:33:44 -0300

    removed css styled components in server-side

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { extractCritical } from 'styled-components'
 
-export default function Document(props) {
+export default function Document() {
 	return (
 		<Html lang='pt-br'>
 			<Head>
@@ -9,7 +8,6 @@ export default function Document(props) {
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="description" content="Loja de Crochê - MãeTerra"></meta>
 				<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;600&family=Oswald:wght@200;400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,500&family=Varela+Round&display=swap" rel="stylesheet" />
-				<style data-emotion-css={props.ids.join(' ')} dangerouslySetInnerHTML={{ __html: props.css }} />
 			</Head>
 			<body>
 				<Main />
@@ -17,9 +15,4 @@ export default function Document(props) {
 			</body>
 		</Html>
 	)
-}
-Document.getInitialProps = async (ctx) => {
-	const initialProps = await Document.getInitialProps(ctx)
-	const styles = extractCritical(initialProps.html)
-	return { ...initialProps, ...styles }
 }
